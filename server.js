5f35d04d61561e98ec652067a27801e9c6948fe3
cmrotruck <crotruck@gmail.com>
2022-01-17 20:22:55 -0500

    added a select all query to the candidates table

--- server.js
+++ server.js
@@ -2,13 +2,32 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mysql = require('mysql2');
+
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to db
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '!September03!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 //default response for any other requests (Not Found)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
 });
 
 app.listen(PORT, () => {
